47d26d2988ea03218ca1416d6078bd6d4617f33b
Sebastian Bean <sebastian@ginlanemedia.com>
2014-02-25 17:27:50 -0500

    helper methods on taxon

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -4,5 +4,19 @@ module Spree
     has_many :ordered_products,->{
       order("#{Spree::GridOrder.table_name}.position ASC")
     }, through: :grid_orders, class_name: "Spree::Product"
+
+    def all_child_products
+      (products + children.includes(:products).map(&:products).flatten.compact).uniq
+    end
+
+    def unordered_products
+      products - ordered_products
+    end
+
+    def ensure_ordered_products
+      unordered_products.each do |p|
+        grid_orders.create product_id:p.id
+      end
+    end
   end
 end
