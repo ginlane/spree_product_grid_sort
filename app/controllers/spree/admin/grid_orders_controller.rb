97c50fca6c3eb753c57ae16f49f58856937cd922
Sebastian Bean <sebastian@ginlanemedia.com>
2014-03-05 14:49:05 -0500

    controller uses classification now

--- app/controllers/spree/admin/grid_orders_controller.rb
+++ app/controllers/spree/admin/grid_orders_controller.rb
@@ -7,14 +7,13 @@ module Spree
         @taxons = Spree::Taxon.all
         if params[:taxon_id]
           @taxon    = Spree::Taxon.find params[:taxon_id]
-          # searcher  = build_searcher taxon: @taxon.id
-          @products = @taxon.ordered_products
+          @products = @taxon.products
         end
       end
 
       def reorder
         reorder_params = JSON.parse params[:reorder]
-        GridOrder.reorder params[:taxon_id], reorder_params
+        Classification.reorder params[:taxon_id], reorder_params
         render json: true
       end
 
