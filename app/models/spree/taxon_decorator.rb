04613882e5d66c1b81ae846d478bd1658d5bb44e
Sebastian Bean <sebastian@ginlanemedia.com>
2014-03-05 14:48:30 -0500

    taxon decore!

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -1,22 +1,22 @@
 module Spree
   Taxon.class_eval do
-    has_many :grid_orders
-    has_many :ordered_products,->{
-      order("#{Spree::GridOrder.table_name}.position ASC")
-    }, through: :grid_orders, class_name: "Spree::Product"
-
-    def all_child_products
-      (products + children.includes(:products).map(&:products).flatten.compact).uniq
-    end
-
-    def unordered_products
-      products - ordered_products
-    end
-
-    def ensure_ordered_products
-      unordered_products.each do |p|
-        grid_orders.create product_id:p.id
+    has_many  :products, through: :taxon_grid, source: :products do
+      def <<(p)
+        proxy_association.owner.taxon_grid.products << p
+      end
+      def push(p)
+        proxy_association.owner.taxon_grid.products << p
       end
     end
+
+    has_many    :taxon_grids, -> {order(:available_on)}
+    belongs_to  :taxon_grid
+    has_many    :available_grids, -> {where(["#{Spree::TaxonGrid.table_name}.available_on < ?",Time.now])}, class_name: 'Spree::TaxonGrid'
+    has_many    :grid_products, through: :taxon_grid, source: :products, class_name: 'Spree::Product'
+
+    after_initialize -> {
+      self.taxon_grids.build(available_on:1.day.ago) if new_record?
+      self.taxon_grid_id ||= (available_grids.last || taxon_grids.last).try :id
+    }
   end
 end
