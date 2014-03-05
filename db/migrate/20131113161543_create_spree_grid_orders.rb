e41c315deee91a36b8e8741d29703af351c2964b
Sebastian Bean <sebastian@ginlanemedia.com>
2014-03-05 14:52:07 -0500

    not necessary anymore

--- db/migrate/20131113161543_create_spree_grid_orders.rb
+++
@@ -1,9 +0,0 @@
-class CreateSpreeGridOrders < ActiveRecord::Migration
-  def change
-    create_table :spree_grid_orders do |t|
-      t.references :taxon
-      t.references  :product
-      t.integer    :position
-    end
-  end
-end
