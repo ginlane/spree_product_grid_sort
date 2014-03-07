c5731a8c61bc810c1880dccbb76a2d0806e51ae9
Sebastian Bean <sebastian@ginlanemedia.com>
2014-03-07 12:11:04 -0500

    controller / helper additions

--- app/controllers/spree/admin/grid_orders_controller.rb
+++ app/controllers/spree/admin/grid_orders_controller.rb
@@ -4,20 +4,44 @@ module Spree
       include Spree::Admin::GridOrdersHelper
       helper 'spree/products'
       def index
-        @taxons = Spree::Taxon.all
-        if params[:taxon_id]
-          @taxon    = Spree::Taxon.find params[:taxon_id]
-          @products = @taxon.products
+        @taxons = Spree::Taxon.order(:parent_id)
+        
+        @taxon = if params[:taxon_id]
+          Spree::Taxon.find params[:taxon_id]
+        else
+          @taxons.first
         end
+
+        @grid = if params[:grid_id]
+          @taxon.grids.find(params[:grid_id])
+        else
+          @taxon.grid
+        end
+        @products = @grid.products
+      
       end
 
       def reorder
         reorder_params = JSON.parse params[:reorder]
-        Classification.reorder params[:taxon_id], reorder_params
+        Classification.reorder params[:grid_id], reorder_params
         render json: true
       end
 
+      def create
+        @taxon    = Spree::Taxon.find params[:taxon_id]
+        @taxon.grids.create grid_params
+        binding.pry
+      end
+
+      def show
+        @grid = TaxonGrid.find params[:id] rescue @taxon.grid
+      end
+
       protected
+
+      def grid_params
+        params.require(:taxon_grid).permit(:available_on, :taxon_id)
+      end
       # TODO: fixme and figure out if this is the best aproach (it's not)
       def current_currency
         'USD'
