2f681121b4c31cc45b2b8eff36a7d9b78fbf151d
Sebastian Bean <sebastian@ginlanemedia.com>
2014-03-17 15:18:43 -0400

    suck it order desc override not working

--- app/models/spree/taxon_grid.rb
+++ app/models/spree/taxon_grid.rb
@@ -9,8 +9,8 @@ class Spree::TaxonGrid < ActiveRecord::Base
     return unless not_me = taxon.taxon_grids.where.not(id:id)
     return unless copy_me = not_me.last
 
-    copy_me.classifications.order(position: :desc).each do |c|
-      classifications.create product_id:c.product_id, position:c.position, taxon_id: c.taxon_id, taxon_grid_id: id
+    copy_me.classifications.reverse_each do |c|
+      classifications.create product_id:c.product_id, taxon_id: c.taxon_id, taxon_grid_id: id
     end
   }
 
