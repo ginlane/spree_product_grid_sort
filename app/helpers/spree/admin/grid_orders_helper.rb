3a0fc122027401cdd3a40647d7e93101d29eb4ef
Braden <braden.douglass@gmail.com>
2016-08-04 12:04:47 -0400

    Linting Helper File
    Small rubocop linting violations cleaned up.

--- app/helpers/spree/admin/grid_orders_helper.rb
+++ app/helpers/spree/admin/grid_orders_helper.rb
@@ -1,10 +1,11 @@
 module Spree::Admin::GridOrdersHelper
-  def taxon_dropdown_menu(taxons, taxon, opt={})
+  def taxon_dropdown_menu(taxons, taxon, opt = {})
     options = options_from_collection_for_select taxons, :id, :short_pretty_name, taxon.try(:id)
 
-    select_tag :taxon_menu, options,  {"data-url" => admin_grid_orders_url}
+    select_tag :taxon_menu, options, "data-url" => admin_grid_orders_url
   end
-  def grids_dropdown_menu(grids, grid, opt={})
+
+  def grids_dropdown_menu(grids, grid, opt = {})
     options = options_from_collection_for_select grids, :id, :pretty_name, grid.try(:id)
     select_tag :grid_id, options, opt
   end
