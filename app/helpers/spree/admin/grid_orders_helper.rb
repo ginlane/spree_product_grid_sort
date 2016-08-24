6e0da60b70a53aae717b05e714e50bae18eeb13d
Braden <braden.douglass@gmail.com>
2016-08-24 17:37:29 -0400

    Add Default To Options Safebuffer
    Instead of relying on the default_blank option to the select tag, build
    our own.
    
    Pivotal:
    https://www.pivotaltracker.com/story/show/128543999

--- app/helpers/spree/admin/grid_orders_helper.rb
+++ app/helpers/spree/admin/grid_orders_helper.rb
@@ -12,7 +12,8 @@ module Spree::Admin::GridOrdersHelper
 
   def retail_store_dropdown_menu(stores, taxon)
     options = options_from_collection_for_select stores, :id, :pretty_name, params['store_id']
+    opt_with_default = '<option value="">All</option>'.html_safe + options
 
-    select_tag :store_menu, options, 'data-url' => admin_retail_grid_orders_url, include_blank: true
+    select_tag :store_menu, opt_with_default, 'data-url' => admin_retail_grid_orders_url
   end
 end
